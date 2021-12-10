adb57d9e4fc54020ae5411b71d51927f5f10e7f0
Psyho <closedeyez@gmail.com>
2021-12-10 13:33:50 +0100

    fix license in setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,14 @@
 from setuptools import setup
 
-with open('LICENSE') as f:
-    license = f.read()
-
 setup(
     name = 'mmtester',
     packages = ['mmtester'],
     package_data = {'mmtester': ['tester.cfg']},
     version = __import__('mmtester').__version__,
-    license = license,
+    license = 'MIT',
     description = "Simple local tester for Topcoder's Marathon contests.",
     author = 'Psyho',
     url = 'https://github.com/FakePsyho/mmtester',
-    download_url = 'https://github.com/FakePsyho/mmtester/archive/refs/tags/v0.1.0.tar.gz',
     keywords = ['Topcoder', 'Marathon', 'Tester'],
     install_requires=['tabulate'],
     classifiers = [
